9351fe8a83efd1aa8f0d19da262f5d8ddbddc661
dylfen <dan@dylfen.dk>
2019-06-19 09:19:24 +0200

    Add files via upload
    Added Active Participant Count, Active Registered Endpoints and Active Registered Users
    fixed minor things

--- SfbCalls.ps1
+++ SfbCalls.ps1
@@ -48,11 +48,12 @@ https://github.com/dylfen
 param (
     [string]$server= "",
     [string]$username= "",
-    [string]$password= ""
+    [string]$password= "" 
 )
 
+	
 #Debug - $true or $false
-$enableDebug =          $false  
+$enableDebug =          $false
 
 # Enable Channels - $true or $false
 $enableSipClients =     $true
@@ -60,6 +61,9 @@ $enableInboundCalls =   $true
 $enableOutboundCalls =  $true
 $enableTotalCalls =     $true
 $enableActiveConfCalls =$true
+$enableConfPaticipants =$true
+$enableActiveRegEndp =	$true
+$enableActiveRegUsers =	$true
 
 
 
@@ -77,82 +81,116 @@ if($enableDebug){
 }
 $result='<?xml version="1.0" encoding="UTF-8" ?><prtg>'
 
-
 foreach ($server in $serverlist){
-    
     if($enableDebug){
         write-host "Getting counters from server:  $server `r`n"
         if($enableSipClients) {
         $value1 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:SIP - Peers(Clients)\SIP - Connections Active"  | select -ExpandProperty CounterSamples)} -Credential $Credentials
-        write-host "SIP Clients returned: " $value1.CookedValue }
+        write-host "SIP Clients returned: " $value1.CookedValue " `r`n"}
         if($enableInboundCalls) {
         $value2 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:MediationServer - Inbound Calls(_Total)\- Current" | select -ExpandProperty CounterSamples)} -Credential $Credentials
-        write-host "Inbound calls returned: " $value2.CookedValue }
+        write-host "Inbound calls returned: " $value2.CookedValue " `r`n"}
         if($enableOutboundCalls) {
         $value3 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:MediationServer - Outbound Calls(_Total)\- Current" | select -ExpandProperty CounterSamples)} -Credential $Credentials
-        write-host "Outbound calls returned: " $value3.CookedValue }
+        write-host "Outbound calls returned: " $value3.CookedValue " `r`n"}
         if($enableTotalCalls) {
         $value4 = $value2.CookedValue + $value3.CookedValue
-        write-host "Total calls (inbound + ourbound): " $value4 }
+        write-host "Total calls (inbound + ourbound): " $value4 " `r`n"}
 		if($enableActiveConfCalls) {
         $value5 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:USrv - Pool Conference Statistics\USrv - Active Conference Count" | select -ExpandProperty CounterSamples)} -Credential $Credentials
-        write-host "Active Conference Calls: " $value5.CookedValue }
+        write-host "Active Conference Calls: " $value5.CookedValue " `r`n"}
+		if($enableConfPaticipants) {
+        $value6 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:USrv - Pool Conference Statistics\USrv - Active Participant Count" | select -ExpandProperty CounterSamples)} -Credential $Credentials
+        write-host "Active Conference Paticipants: " $value6.CookedValue " `r`n"}
+		if($enableActiveRegEndp) {
+        $value7 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:USrv - Endpoint Cache\USrv - Active Registered Endpoints" | select -ExpandProperty CounterSamples)} -Credential $Credentials
+        write-host "Active Registered Endpoints: " $value7.CookedValue " `r`n"}
+		if($enableActiveRegUsers) {
+        $value8 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:USrv - Endpoint Cache\USrv - Active Registered Users" | select -ExpandProperty CounterSamples)} -Credential $Credentials
+        write-host "Active Registered Users: " $value8.CookedValue " `r`n"}
     } else {
-        $value1 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:SIP - Peers(Clients)\SIP - Connections Active" -ErrorAction silentlycontinue | select -ExpandProperty CounterSamples)} -Credential $Credentials
-        $value2 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:MediationServer - Inbound Calls(_Total)\- Current" -ErrorAction silentlycontinue | select -ExpandProperty CounterSamples)} -Credential $Credentials
-        $value3 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:MediationServer - Outbound Calls(_Total)\- Current" -ErrorAction silentlycontinue | select -ExpandProperty CounterSamples)} -Credential $Credentials
-        $value4 = $value2.CookedValue + $value3.CookedValue
-        $value5 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:USrv - Pool Conference Statistics\USrv - Active Conference Count" -ErrorAction silentlycontinue | select -ExpandProperty CounterSamples)} -Credential $Credentials
+        if($enableSipClients) 		{ $value1 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:SIP - Peers(Clients)\SIP - Connections Active" -ErrorAction silentlycontinue  | select -ExpandProperty CounterSamples)} -Credential $Credentials }
+		if($enableInboundCalls) 	{ $value2 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:MediationServer - Inbound Calls(_Total)\- Current" -ErrorAction silentlycontinue | select -ExpandProperty CounterSamples)} -Credential $Credentials }
+		if($enableOutboundCalls) 	{ $value3 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:MediationServer - Outbound Calls(_Total)\- Current" -ErrorAction silentlycontinue | select -ExpandProperty CounterSamples)} -Credential $Credentials }
+		if($enableTotalCalls) 		{ $value4 = $value2.CookedValue + $value3.CookedValue }
+		if($enableActiveConfCalls) 	{ $value5 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:USrv - Pool Conference Statistics\USrv - Active Conference Count" -ErrorAction silentlycontinue | select -ExpandProperty CounterSamples)} -Credential $Credentials }
+		if($enableConfPaticipants) 	{ $value6 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:USrv - Pool Conference Statistics\USrv - Active Paticipant Count" -ErrorAction silentlycontinue | select -ExpandProperty CounterSamples)} -Credential $Credentials }
+		if($enableActiveRegEndp) 	{ $value7 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:USrv - Endpoint Cache\USrv - Active Registered Endpoints" -ErrorAction silentlycontinue | select -ExpandProperty CounterSamples)} -Credential $Credentials }
+		if($enableActiveRegUsers) 	{ $value8 = Invoke-Command -Computername $server -ScriptBlock {(Get-Counter "\LS:USrv - Endpoint Cache\USrv - Active Registered Users" -ErrorAction silentlycontinue | select -ExpandProperty CounterSamples)} -Credential $Credentials }
     }
+
 	if ($error) {
         if($enableDebug){
         # Skip Counters
-            write-host "Server: $server Unable to read $counter `r`n"
+            write-host "Server: $server Unable to read one or more counter(s) `r`n"
         }
 		$error.clear()
 	}
 	else  {
         if($enableSipClients) {
-            $result+="  <result>"
-            $result+="    <channel>SIP Clients("+$server+")</channel>"
-            $result+="    <value>"+$value1.CookedValue+"</value>"
-            $result+="    <unit>Count</unit>"
-            $result+="    <mode>Absolute</mode>"
-            $result+="  </result>"
+            $result+="  <result>`r`n"
+            $result+="    <channel>SIP Clients("+$server+")</channel>`r`n"
+            $result+="    <value>"+$value1.CookedValue+"</value>`r`n"
+            $result+="    <unit>Count</unit>`r`n"
+            $result+="    <mode>Absolute</mode>`r`n"
+            $result+="  </result>`r`n"
             }
         if($enableInboundCalls) {
-            $result+="  <result>"
-            $result+="    <channel>Inbound Calls("+$server+")</channel>"
-            $result+="    <value>"+$value2.CookedValue+"</value>"
-            $result+="    <unit>Count</unit>"
-            $result+="    <mode>Absolute</mode>"
-            $result+="  </result>"
+            $result+="  <result>`r`n"
+            $result+="    <channel>Inbound Calls("+$server+")</channel>`r`n"
+            $result+="    <value>"+$value2.CookedValue+"</value>`r`n"
+            $result+="    <unit>Count</unit>`r`n"
+            $result+="    <mode>Absolute</mode>`r`n"
+            $result+="  </result>`r`n"
         }
         if($enableOutboundCalls) {
-            $result+="  <result>"
-            $result+="    <channel>Outbound Calls("+$server+")</channel>"
-            $result+="    <value>"+$value3.CookedValue+"</value>"
-            $result+="    <unit>Count</unit>"
-            $result+="    <mode>Absolute</mode>"
-            $result+="  </result>"
+            $result+="  <result>`r`n"
+            $result+="    <channel>Outbound Calls("+$server+")</channel>`r`n"
+            $result+="    <value>"+$value3.CookedValue+"</value>`r`n"
+            $result+="    <unit>Count</unit>`r`n"
+            $result+="    <mode>Absolute</mode>`r`n"
+            $result+="  </result>`r`n"
         }
         if($enableTotalCalls) {
-            $result+="  <result>"
-            $result+="    <channel>Total Calls("+$server+")</channel>"
-            $result+="    <value>"+$value4+"</value>"
-            $result+="    <unit>Count</unit>"
-            $result+="    <mode>Absolute</mode>"
-            $result+="  </result>"
+            $result+="  <result>`r`n"
+            $result+="    <channel>Total Calls("+$server+")</channel>`r`n"
+            $result+="    <value>"+$value4+"</value>`r`n"
+            $result+="    <unit>Count</unit>`r`n"
+            $result+="    <mode>Absolute</mode>`r`n"
+            $result+="  </result>`r`n"
         }
         if($enableActiveConfCalls) {
-            $result+="  <result>"
-            $result+="    <channel>Active Conference Calls("+$server+")</channel>"
-            $result+="    <value>"+$value5.CookedValue+"</value>"
-            $result+="    <unit>Count</unit>"
-            $result+="    <mode>Absolute</mode>"
-            $result+="  </result>"
+            $result+="  <result>`r`n"
+            $result+="    <channel>Active Conference Calls("+$server+")</channel>`r`n"
+            $result+="    <value>"+$value5.CookedValue+"</value>`r`n"
+            $result+="    <unit>Count</unit>`r`n"
+            $result+="    <mode>Absolute</mode>`r`n"
+            $result+="  </result>`r`n"
         }
-
+		if($enableConfPaticipants) {
+            $result+="  <result>`r`n"
+            $result+="    <channel>Active Conference Paticipants("+$server+")</channel>`r`n"
+            $result+="    <value>"+$value6.CookedValue+"</value>`r`n"
+            $result+="    <unit>Count</unit>`r`n"
+            $result+="    <mode>Absolute</mode>`r`n"
+            $result+="  </result>`r`n"
+        }
+		if($enableActiveRegEndp) {
+            $result+="  <result>`r`n"
+            $result+="    <channel>Active Registered Endpoints("+$server+")</channel>`r`n"
+            $result+="    <value>"+$value7.CookedValue+"</value>`r`n"
+            $result+="    <unit>Count</unit>`r`n"
+            $result+="    <mode>Absolute</mode>`r`n"
+            $result+="  </result>`r`n"
+        }
+		if($enableActiveRegUsers) {
+            $result+="  <result>`r`n"
+            $result+="    <channel>Active Registered Users("+$server+")</channel>`r`n"
+            $result+="    <value>"+$value8.CookedValue+"</value>`r`n"
+            $result+="    <unit>Count</unit>`r`n"
+            $result+="    <mode>Absolute</mode>`r`n"
+            $result+="  </result>`r`n"
+        }
 	}
 }
 
